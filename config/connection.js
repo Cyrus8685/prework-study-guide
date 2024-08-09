f7db29dab84e0395c76438e8f32471201ebce56f
Jonathan Negron <jonathan.a.negron@gmail.com>
2024-08-09 01:25:13 -0400

    Got Api Calls Working 01

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const sequelize = new Sequelize({
   database: "cyrus",
   host: "localhost",
-  port: 5432,
+  port: 10000,
   dialect: "postgres",
 });
     module.exports = sequelize
